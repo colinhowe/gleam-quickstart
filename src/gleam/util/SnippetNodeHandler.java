5fb31ec9fb1deeed5d9f20b7b45f1ce766d7c147
colinhowe <colinthehowe@gmail.com>
2010-04-25 11:25:56 +0100

    Add copyright notice to all source files

--- src/gleam/util/SnippetNodeHandler.java
+++ src/gleam/util/SnippetNodeHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010 Colin Howe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gleam.util;
 
 import java.util.HashMap;
@@ -19,10 +35,13 @@ public class SnippetNodeHandler implements NodeHandler {
   public void handle(Node node, StringBuilder builder) {
     final Map<String, String> attributes = new HashMap<String, String>();
     
+    // Copy all the attributes from the current node onto the attributes map
     for (Entry<String, Object> entry : node.getAttributes().entrySet()) {
       attributes.put(entry.getKey(), entry.getValue().toString());
     }
       
+    // Set the inner value for this snippet as either the inner nodes or the
+    // value of the current node
     if (node.getNodes() != null && node.getNodes().size() != 0) {
       final StringBuilder innerBuilder = new StringBuilder();
       for (final Node innerNode : node.getNodes()) {
@@ -33,6 +52,7 @@ public class SnippetNodeHandler implements NodeHandler {
       attributes.put("_inner", node.getValue().toString());
     }
     
+    // Process the snippet into HTML and put on the string builder
     builder.append(snippetProcessor.process(
       SnippetProvider.loadSnippet(snippetName).getContents(), attributes));
   }
--- src/gleam/util/SnippetProcessor.java
+++ src/gleam/util/SnippetProcessor.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2010 Colin Howe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gleam.util;
 
 import java.util.Map;
 
 public class SnippetProcessor {
   public String process(final String snippet, final Map<String, String> substitutions) {
+    
+    // Replace all the % tokens with the appropriate value from the substitutions map
+    // Any double % signs (%%) will be replaced with a % sign in the created HTML
     final StringBuilder buffer = new StringBuilder();
     int currentIndex = 0;
     while (currentIndex < snippet.length()) {
